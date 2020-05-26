36f4ec7125e0f981763a3ce578f39ea13af4b519
YP <961204yp@gmail.com>
2020-05-26 17:36:37 +0100

    Node.js Initial

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,11 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+router.get('/homepage', function(req, res, next) {
+  res.render('homepage');
+});
+
+
+
 module.exports = router;

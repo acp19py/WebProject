42e3de0e526578bcdd3df6cb81c78335c588dcc6
acq19cy <acq19cy@sheffield.ac.uk>
2020-05-26 17:54:01 +0100

    router changes

--- routes/index.js
+++ routes/index.js
@@ -11,6 +11,14 @@ router.get('/homepage', function(req, res, next) {
   res.render('homepage');
 });
 
+router.get('/create_animal', function(req, res, next) {
+  res.render('create_animal');
+});
+
+router.get('/animal_display', function(req, res, next) {
+  res.render('animal_display');
+});
+
 router.get('/thankyou_page', function(req, res, next) {
   res.render('thankyou_page');
 });

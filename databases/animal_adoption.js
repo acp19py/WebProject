8164da0c8db5ab78d3b0da6adf79fc77879e8751
YP <961204yp@gmail.com>
2020-05-26 18:38:08 +0100

    Mongodb Schema initial

--- databases/animal_adoption.js
+++ databases/animal_adoption.js
@@ -4,7 +4,7 @@ var mongoose = require('mongoose');
 
 //The URL which will be queried. Run "mongod.exe" for this to connect
 //var url = 'mongodb://localhost:27017/test';
-var mongoDB = 'mongodb+srv://user1:45vduJLy32NrJ3pN@cluster0-epzou.mongodb.net/test?retryWrites=true&w=majority/test';
+var mongoDB = 'mongodb+srv://user1:45vduJLy32NrJ3pN@cluster0-epzou.mongodb.net/test?retryWrites=true&w=majority/animaladoption';
 
 mongoose.Promise = global.Promise;
 mongoose.connect(mongoDB);
--- models/animal_adoption.js
+++ models/animal_adoption.js
@@ -1,3 +1,56 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+const commentSchema = new Schema({
+    Name: String,
+    Content: String,
+    Date: Date,
+});
+
+const comment = mongoose.model('Comment', commentSchema);
+
+const animalSchema = new Schema({
+    Type: String,
+    Gender: String,
+    Kind: String,
+    Color: String,
+    Age: Number,
+    Location: String,
+    Status: String,
+    Vaccine: Boolean,
+    Description: String,
+    Comment:[commentSchema],
+});
+
+const animals = mongoose.model('animals',animalSchema);
+
+const userSchema = new Schema({
+    Name: String,
+    Email: String,
+    Password: String,
+    Phone: String,
+    AllForm: [{type:Schema.Types.ObjectId,ref:'adoptionforms'}],
+});
+
+const users = mongoose.model('users',userSchema);
+
+const adoptionformSchema = new Schema({
+    UserID:{type: Schema.Types.ObjectId,ref:'users'},
+    AnimalID:{type:Schema.Types.ObjectId,ref:'animals'},
+    Location: String,
+    Experience: String,
+    Job: String,
+    Status: String,
+    AdoptReason: String,
+    Age: Number,
+    Income: Number,
+});
+
+const adoptionforms = mongoose.model('adoptionforms',adoptionformSchema);
+
+module.exports={
+    comment,
+    animals,
+    users,
+    adoptionforms,
+};
